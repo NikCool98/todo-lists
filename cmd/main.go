92ed33d143668568409d7248b01e35101e34f09b
NikKool98 <shepard1998@mail.ru>
2024-02-06 01:24:17 +0300

    ÐFourth commit

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"github.com/NikCool98/todo-lists"
+	"github.com/NikCool98/todo-lists/pkg/handler"
+	"github.com/NikCool98/todo-lists/pkg/repository"
+	"github.com/NikCool98/todo-lists/pkg/service"
 	"log"
 )
 
 func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 
